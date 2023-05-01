051be17911753dd7e4053df79fef5223b9bbf7bc
Hyeok <lyhyeok1015@gnu.ac.kr>
2023-05-01 21:00:54 +0900

    LibraryManagement

--- src/MenuManeger.java
+++ src/MenuManeger.java
@@ -4,37 +4,35 @@ public class MenuManeger {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=0;
-		Scanner input=new Scanner(System.in);
 		
+		
+		Scanner input=new Scanner(System.in);
+		BookManager bookManger=new BookManager(input);
+		int num=0;
 				
-		while(num!=6) {
+		while(num!=5) {
+		System.out.println();
 		System.out.println("1.Adding a book");
 		System.out.println("2.Delete a book");
 		System.out.println("3.borrowing a book");
-		System.out.println("4.Return a book");
-		System.out.println("5.Show a menu");
-		System.out.println("6.Exit");
+		System.out.println("4.Return a book");		
+		System.out.println("5.Exit");
 		System.out.println("Select one number between 1-5:");
 		num=input.nextInt();
-		switch(num) {
-		case 1:
-			System.out.print("Book Name:");
-			String Bookname=input.nextLine();
-			input.nextLine();
-			System.out.print("Book Author:");
-			String BookAuthor=input.nextLine();
-			System.out.print("Book Publisher:");
-			String BookPublisher=input.nextLine();
-			break;
-		case 2:
-		case 3:
-		case 4:
-			
-			
-		
+		if(num==1) {
+			bookManger.addBook();
+		}	
+		else if(num==2) {
+			bookManger.deleteBook();
+		}	
+		else if(num==3) {
+			bookManger.borrowingBook();
+		}	
+		else if(num==4) {
+			bookManger.viewBook();
 		}
-		}
+		else continue;
 	}
-
+}
+	
 }
