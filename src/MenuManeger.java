aa67acd88c15e6d3d7888a111ca77e1661981102
Hyeok <lyhyeok1015@gnu.ac.kr>
2023-05-01 14:31:25 +0900

    removed and added

---
+++ src/MenuManeger.java
@@ -0,0 +1,14 @@
+
+public class MenuManeger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1.Finding a book");
+		System.out.println("2.borrowing a book");
+		System.out.println("3.Return a book");
+		System.out.println("4.Adding a book");
+		System.out.println("5.Exit");
+
+	}
+
+}
