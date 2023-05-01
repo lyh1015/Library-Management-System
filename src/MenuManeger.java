1e6b4bd48a3967c18199ffff341c7336dbb852fa
Hyeok <lyhyeok1015@gnu.ac.kr>
2023-05-01 15:09:55 +0900

    add menu selection

--- src/MenuManeger.java
+++ src/MenuManeger.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
 
 public class MenuManeger {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1.Finding a book");
-		System.out.println("2.borrowing a book");
-		System.out.println("3.Return a book");
-		System.out.println("4.Adding a book");
-		System.out.println("5.Exit");
-
+		int num=0;
+		Scanner input=new Scanner(System.in);
+		
+				
+		while(num!=6) {
+		System.out.println("1.Adding a book");
+		System.out.println("2.Delete a book");
+		System.out.println("3.borrowing a book");
+		System.out.println("4.Return a book");
+		System.out.println("5.Show a menu");
+		System.out.println("6.Exit");
+		System.out.println("Select one number between 1-5:");
+		num=input.nextInt();
+		if(num==1) {
+			System.out.print("Book Name:");
+			String Bookname=input.nextLine();
+			input.nextLine();
+			System.out.print("Book Author:");
+			String BookAuthor=input.nextLine();
+			System.out.print("Book Publisher:");
+			String BookPublisher=input.nextLine();
+			
+			
+		}
+		}
 	}
 
 }
